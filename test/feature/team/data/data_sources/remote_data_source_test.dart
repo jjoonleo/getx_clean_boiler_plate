533b40e79dc2684dabae8f6da373b4b8ec153dd2
Ejun Park <jjoonleo@gmail.com>
2023-12-21 16:07:48 +0900

    create test to test getTeamModels return a server exception when the reponse is 404 or other

--- test/feature/team/data/data_sources/remote_data_source_test.dart
+++ test/feature/team/data/data_sources/remote_data_source_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:getx_clean_boiler_plate/core/constants/constants.dart';
+import 'package:getx_clean_boiler_plate/core/error/exception.dart';
 import 'package:getx_clean_boiler_plate/feature/team/data/data_sources/remote_data_source.dart';
 import 'package:getx_clean_boiler_plate/feature/team/data/models/team_model.dart';
 import 'package:mockito/mockito.dart';
@@ -32,4 +33,23 @@ void main() {
 
       expect(result, isA<List<TeamModel>>());
     });
+
+    test('should return a server exception when the response is 404 or other', () async {
+      when(
+        mockHttpClient.get(Uri.parse(Urls.allTeams))
+      ).thenAnswer((_) async =>
+        http.Response(
+            'Not found',
+            404
+          )
+        );
+
+      expect(() async => await teamRemoteDataSource.getTeamModels(), throwsA(isA<ServerException>()));
+      
+
+      
+    });
+    
+  });
+  
 }
