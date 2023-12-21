2d42a7660cb87618b199f292b55306de61ad069c
Ejun Park <jjoonleo@gmail.com>
2023-12-21 11:45:30 +0900

    create team model

---
+++ lib/feature/team/data/models/team_model.dart
@@ -0,0 +1,10 @@
+import 'package:getx_clean_boiler_plate/feature/team/domain/entities/team.dart';
+
+class TeamModel extends TeamEntity {
+  const TeamModel({
+    required String name,
+  }) : super(
+          name: name,
+        );
+
+}
