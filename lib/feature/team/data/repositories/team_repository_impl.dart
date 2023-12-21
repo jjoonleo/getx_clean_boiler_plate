f8793413a3aa9ed6cf9fa4564d117d91b2318a85
Ejun Park <jjoonleo@gmail.com>
2023-12-21 17:49:29 +0900

    implement getTeams method in TeamRepository class

--- lib/feature/team/data/repositories/team_repository_impl.dart
+++ lib/feature/team/data/repositories/team_repository_impl.dart
@@ -13,5 +13,13 @@ class TeamRepositoryImpl extends TeamRepository {
 
   @override
   Future<Either<Failure, List<TeamEntity>>> getTeams() async {
+    try {
+      final result = await teamRemoteDataSource.getTeamModels();
+      return Right(result.map((e) => e.toEntity()).toList());
+    } on ServerException {
+      return const Left(ServerFailure('An error has occurred'));
+    } on SocketException {
+      return const Left(ServerFailure('Failed to connect to the network'));
+    }
   }
 }
