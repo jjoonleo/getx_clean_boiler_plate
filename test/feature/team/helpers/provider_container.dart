402684cfa449fcf1147e8ec0b9182e09483f04b0
Ejun Park <jjoonleo@gmail.com>
2023-12-21 23:18:54 +0900

    create riverpod container for testing

---
+++ test/feature/team/helpers/provider_container.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:riverpod/riverpod.dart';
+
+ProviderContainer createContainer({
+  ProviderContainer? parent,
+  List<Override> overrides = const [],
+  List<ProviderObserver>? observers,
+}) {
+  final container = ProviderContainer(
+    parent: parent,
+    overrides: overrides,
+    observers: observers,
+  );
+
+  addTearDown(container.dispose);
+
+  return container;
+}
